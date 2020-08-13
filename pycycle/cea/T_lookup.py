1764ad410400df7997f67a3991cb807e53e6fe35
Jennifer Bergeson <jberges@purdue.edu>
2020-08-13 14:28:20 -0400

    Revert "replaced Cp lookup and h lookup with constant Cp formulation. Replaced S lookup as well, but complications arise which need fixing in the pressure solve using S"
    This reverts commit d339c2a8c177b59160c3ac38e99d64e812680ad2.

--- pycycle/cea/T_lookup.py
+++ pycycle/cea/T_lookup.py
@@ -1,5 +1,4 @@
 import openmdao.api as om
-import numpy as np
 
 # from pycycle.cea.species_data import Thermo
 from pycycle.cea.explicit_isentropic import ExplicitIsentropic
@@ -40,90 +39,36 @@ class TempFromEnthalpy(om.ExplicitComponent):
         J['T', 'h'] = 1/Cp
 
 
-
-class TempFromEntropy(om.ExplicitComponent):
-
-    def initialize(self):
-        self.options.declare('T_base', default=302.4629819, desc='base temperature (units are degK)')
-        self.options.declare('P_base', default=1.013, desc='base pressure (units are bar)')
-        self.options.declare('S_base', default=1.64322352, desc='base entropy (units are cal/(g*degK)')
-
-    def setup(self):
-        self.add_input('Cp', units='cal/(g*degK)', desc='specific heat (assumed constant)')
-        self.add_input('S', units='cal/(g*degK)', desc='entropy at input pressure and output temperature')
-        self.add_input('R', units='cal/(g*degK)', desc='specific gas constant')
-        self.add_input('P', units='bar', desc='pressure to find temperature at')
-
-        self.add_output('T', units='degK', desc='temperature at specified pressure and entropy')
-
-        self.declare_partials('T', ('Cp', 'S', 'R', 'P'))
-
-    def compute(self, inputs, outputs):
-
-        T_base = self.options['T_base']
-        P_base = self.options['P_base']
-        S_base = self.options['S_base']
-
-        Cp = inputs['Cp']
-        S = inputs['S']
-        R = inputs['R']
-        P = inputs['P']
-
-        outputs['T'] = T_base*np.exp((1/Cp)*(S - S_base + R*np.log(P/P_base)))
-
-    def compute_partials(self, inputs, J):
-
-        T_base = self.options['T_base']
-        P_base = self.options['P_base']
-        S_base = self.options['S_base']
-
-        Cp = inputs['Cp']
-        S = inputs['S']
-        R = inputs['R']
-        P = inputs['P']
-
-        T = T_base*np.exp((1/Cp)*(S - S_base + R*np.log(P/P_base)))
-
-        J['T', 'Cp'] = T*(-1/Cp**2)*(S - S_base + R*np.log(P/P_base))
-        J['T', 'S'] = T/Cp
-        J['T', 'R'] = T*np.log(P/P_base)
-        J['T', 'P'] = T*R/P
-
-
-
 class TLookup(om.Group):
 
     def initialize(self):
         self.options.declare('mode', values=('h', 'S'), desc='switch to tell whether to look up Cp')
+        self.options.declare('S_data', default=None, desc='thermodynamic property data')
         self.options.declare('h_base', default=0.0, desc='enthalpy at base temperature (units are cal/g)')
         self.options.declare('T_base', default=302.4629819, desc='base temperature (units are degK)')
-        self.options.declare('P_base', default=1.013, desc='base pressure (units are bar)')
-        self.options.declare('S_base', default=1.64322352, desc='base entropy (units are cal/(g*degK)')
 
     def setup(self):
 
         comp_mode = self.options['mode']
-        T_base = self.options['T_base']
+        S_data = self.options['S_data']
 
         if comp_mode == 'h':
 
             h_base=self.options['h_base']
+            T_base = self.options['T_base']
 
             self.add_subsystem('h_table', TempFromEnthalpy(h_base=h_base, T_base=T_base),
                 promotes_inputs=('h', 'Cp'), promotes_outputs=('T',))
 
         else:
 
-            P_base = self.options['P_base']
-            S_base = self.options['S_base']
+            if S_data is None:
+                raise ValueError('You have not provided data to PressureSolve and it is required')
 
-            # self.add_subsystem('S_table', properties.PropertyMap(map_data=S_data), promotes_inputs=('P', 'T'), promotes_outputs=(('S', 'S_calculated'),))
+            self.add_subsystem('S_table', properties.PropertyMap(map_data=S_data), promotes_inputs=('P', 'T'), promotes_outputs=(('S', 'S_calculated'),))
 
-            # self.add_subsystem('entropy_matching', om.BalanceComp('T', units='degK', eq_units='cal/(g*degK)'), promotes_outputs=('T', ), promotes_inputs=(('lhs:T', 'S'),))
-            # self.connect('S_calculated', 'entropy_matching.rhs:T')
-
-            self.add_subsystem('S_table', TempFromEntropy(T_base=T_base, P_base=P_base, S_base=S_base), 
-                promotes_inputs=('P', 'S', 'Cp', 'R'), promotes_outputs=('T',))
+            self.add_subsystem('entropy_matching', om.BalanceComp('T', units='degK', eq_units='cal/(g*degK)'), promotes_outputs=('T', ), promotes_inputs=(('lhs:T', 'S'),))
+            self.connect('S_calculated', 'entropy_matching.rhs:T')
 
     def configure(self):
 
@@ -161,12 +106,13 @@ if __name__ == "__main__":
     import numpy as np
 
     S_data = properties.AIR_MIX_entropy
+    h_data = properties.AIR_MIX_enthalpy
 
     prob = om.Problem()
 
 
-    prob.model = TLookup(mode='S', S_data=S_data)
-    # prob.model.set_input_defaults('h', -0.59153318, units='cal/g')
+    prob.model = TLookup(mode='h', data=h_data)
+    prob.model.set_input_defaults('h', -0.59153318, units='cal/g')
 
     # prob.model = TLookup(mode='S', data=S_data)
     # prob.model.set_input_defaults('P', 1.2, units='bar')
@@ -189,6 +135,8 @@ if __name__ == "__main__":
 
     prob.run_model()
     prob.check_partials(method='cs', compact_print=True)
+    print(prob['h'])
+    print(prob['T'])
     # print(prob['P'])
     # print(prob['S_calculated'])
     # print(prob['S'])
@@ -198,7 +146,7 @@ if __name__ == "__main__":
 
     p = om.Problem()
     p.model = om.Group()
-    p.model.add_subsystem('temp', TempFromEntropy(), promotes=['*'])
+    p.model.add_subsystem('temp', TempFromEnthalpy(), promotes=['*'])
     p.setup(force_alloc_complex=True)
     p.run_model()
     p.check_partials(method='cs', compact_print=True)
--- pycycle/cea/pressure_solve.py
+++ pycycle/cea/pressure_solve.py
@@ -2,8 +2,9 @@ import openmdao.api as om
 
 # from pycycle.cea.species_data import Thermo
 from pycycle.cea.explicit_isentropic import ExplicitIsentropic
+import pycycle.cea.properties as properties
+from pycycle.cea.thermo_lookup import ThermoLookup
 from pycycle.cea import properties
-from pycycle.cea.thermo_lookup import EntropyFromTemp
 
 
 class PressureSolve(om.Group):
@@ -11,17 +12,11 @@ class PressureSolve(om.Group):
     def initialize(self):
         self.options.declare('mode', default='T', values=('T', 'h', 'S'), desc='mode to calculate in')
         self.options.declare('S_data', default=None, desc='thermodynamic property data')
-        self.options.declare('T_base', default=302.4629819, desc='base temperature (units are degK)')
-        self.options.declare('P_base', default=1.013, desc='base pressure (units are bar)')
-        self.options.declare('S_base', default=1.64322352, desc='base entropy (units are cal/(g*degK)')
 
     def setup(self):
 
         mode = self.options['mode']
         S_data = self.options['S_data']
-        T_base = self.options['T_base']
-        P_base = self.options['P_base']
-        S_base = self.options['S_base']
 
         if mode == 'T' or mode == 'h':
             self.add_subsystem('isentropic_pressure', om.ExecComp('P=Pt*(1 + (gamma - 1)/2 * MN**2)**(-gamma/(gamma - 1))',
@@ -30,24 +25,18 @@ class PressureSolve(om.Group):
 
             self.add_subsystem('S_table', properties.PropertyMap(map_data=S_data), promotes_inputs=('P', 'T'), promotes_outputs=('S',))
             self.add_subsystem('St_table', properties.PropertyMap(map_data=S_data), promotes_inputs=(('P', 'Pt'), ('T', 'Tt')), promotes_outputs=(('S', 'St'),))
-            ## the following impelementation was written to replace the maps, but is ill-defined and needs reworking
-            # self.add_subsystem('S_table', EntropyFromTemp(T_base=T_base, P_base=P_base, S_base=S_base
-            #     ), promotes_inputs=('P', 'T'), promotes_outputs=('S',))
-            # self.add_subsystem('St_table', EntropyFromTemp(T_base=T_base, P_base=P_base, S_base=S_base
-            #     ), promotes_inputs=(('P', 'Pt'), ('T', 'Tt')), promotes_outputs=(('S', 'St'),))
             
 
-            self.add_subsystem('entropy_matching', om.BalanceComp('Pt', units='bar', eq_units='cal/(g*degK)', lower=1e-2, res_ref=.001), promotes_outputs=('Pt',))
+            self.add_subsystem('entropy_matching', om.BalanceComp('Pt', units='bar', eq_units='cal/(g*degK)'), promotes_outputs=('Pt',))
             self.connect('S', 'entropy_matching.lhs:Pt')
             self.connect('St', 'entropy_matching.rhs:Pt')
 
-        else: #this can be replaced with a pressure lookup using constant Cp
-            self.add_subsystem('entropy_matching', om.BalanceComp('P', units='bar', eq_units='cal/(g*degK)', lower=1e-5), promotes_outputs=('P',), promotes_inputs=(('lhs:P', 'S_desired'),))
+        else:
+            self.add_subsystem('entropy_matching', om.BalanceComp('P', units='bar', eq_units='cal/(g*degK)'), promotes_outputs=('P',), promotes_inputs=(('lhs:P', 'S_desired'),))
             self.connect('S_actual', 'entropy_matching.rhs:P')
 
-            # self.add_subsystem('S_table', properties.PropertyMap(map_data=S_data), promotes_inputs=('P', 'T'), promotes_outputs=(('S', 'S_actual'),))
-            self.add_subsystem('S_table', EntropyFromTemp(T_base=T_base, P_base=P_base, S_base=S_base
-                ), promotes_inputs=('P', 'T'), promotes_outputs=(('S', 'S_actual'),))
+            self.add_subsystem('S_table', properties.PropertyMap(map_data=S_data), promotes_inputs=('P', 'T'), promotes_outputs=(('S', 'S_actual'),))
+
 
     def configure(self):
 
@@ -62,9 +51,6 @@ class PressureSolve(om.Group):
 
         self.options['assembled_jac_type'] = 'dense'
         newton.linear_solver = om.DirectSolver(assemble_jac=True)
-        if self.options['mode'] != 'S':
-            newton.linear_solver = om.LinearRunOnce()
-            # newton.linear_solver.options['maxiter'] = 10
 
         ln_bt = newton.linesearch = om.BoundsEnforceLS()
         ln_bt.options['bound_enforcement'] = 'scalar'
